31587faa4e4711f849dc87e47a8cd99b54ba77c5
gowrikandasamy <45310762+gowrikandasamy@users.noreply.github.com>
2024-09-03 18:20:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,8 @@
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
+variable "ingress_cidr_blocks" {}
 variable "avail_zone" {}
 variable "env_prefix" {}  # To define or differentate the types of env Dev, QA, Pre-prod, Prod
 variable "instance_type" {}
+variable "public_subnets" {}
+variable "private_subnets" {}
