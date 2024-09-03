4b24b96a8d6b9c5e247bbc07e99c23610a3a2112
gowrikandasamy <45310762+gowrikandasamy@users.noreply.github.com>
2024-09-03 18:25:40 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,12 @@
-resource "aws_vpc" "myapp-vpc" {
+resource "aws_vpc" "demo-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
     Name = "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id            = aws_vpc.myapp-vpc.id
+resource "aws_subnet" "demo-subnet-1" {
+  vpc_id            = aws_vpc.demo-vpc.id
   cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
   tags = {
@@ -14,7 +14,7 @@ resource "aws_subnet" "myapp-subnet-1" {
   }
 }
 
-resource "aws_internet_gateway" "myapp-igw" {
+resource "aws_internet_gateway" "demo-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
   tags = {
     Name = "${var.env_prefix}-igw"
@@ -22,18 +22,18 @@ resource "aws_internet_gateway" "myapp-igw" {
 }
 
 resource "aws_default_route_table" "main-rtb" {
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  default_route_table_id = aws_vpc.demo-vpc.default_route_table_id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
+    gateway_id = aws_internet_gateway.demo-igw.id
   }
   tags = {
     Name = "${var.env_prefix}-main-rtb"
   }
 }
 
-resource "aws_default_security_group" "default-sg" {
-  vpc_id = aws_vpc.myapp-vpc.id
+resource "aws_default_security_group" "demo-sg" {
+  vpc_id = aws_vpc.demo-vpc.id
   ingress {
     from_port   = 22
     to_port     = 22
@@ -53,6 +53,6 @@ resource "aws_default_security_group" "default-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "${var.env_prefix}-default-sg"
+    Name = "${var.env_prefix}-demo-sg"
   }
 }
