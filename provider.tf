21142d75e6300158a4e213c758e577ea0f70d9db
gowrikandasamy <45310762+gowrikandasamy@users.noreply.github.com>
2024-09-03 18:26:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.52.0"
+      version >= "5.65.0"
     }
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
