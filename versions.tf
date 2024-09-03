90d03c318bcc3e9f5b4a6951e071b6947cf36e60
gowrikandasamy <45310762+gowrikandasamy@users.noreply.github.com>
2024-09-03 18:01:40 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.9.5"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = ">= 5.65.0"
     }
   }
 }
